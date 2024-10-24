bb8d2c2a0d4a8d6ff02d598e1d0b15c1df0c307e
tanvi-2103-git <tanvidudam0@gmail.com>
2024-10-24 18:34:09 +0530

    create one model
    tiffin model created

---
+++ model/tiffinItemModel.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+export interface TiffinItem extends Document {
+
+    tiffin_image_url: string; 
+    tiffin_name: string;
+    tiffin_available_quantity: number; 
+    tiffin_description: string;
+    retailer_id: string;
+    tiffin_type: ['veg','non-veg'];
+    // tiffin_category: ['chinese', '']
+    tiffin_price: number;
+    tiffin_rating: number;
+    tiffin_isavailable: boolean;
+    tiffin_created_at:Date;
+    tiffin_updated_at:Date;
+
+  }
+ 
+  const TiffinItemSchema = new mongoose.Schema({
+    tiffin_image_url: { type: String},
+    tiffin_name: { type: String, required: true },
+    tiffin_available_quantity: { type: Number, required: true },
+    tiffin_description: { type: String}, 
+    retailer_id: { type: Number, required: true },
+    tiffin_type: { type: String, enum: ['veg','non-veg'],required: true},
+    tiffin_price: { type: Number, required: true },
+    tiffin_rating: { type: Number, required: true },
+    tiffin_isavailable: { type: Boolean, required: true },
+    tiffin_created_at:{ type: Date, required: true },
+    tiffin_updated_at:{ type: Date, required: true },
+
+    
+});
+
+export const TiffinItemModel = mongoose.model('TiffinItem', TiffinItemSchema);
