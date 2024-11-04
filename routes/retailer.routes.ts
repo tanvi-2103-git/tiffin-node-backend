8a8e90681e1aa13fbff959ff7ae585bdefaed446
tanvi-2103-git <tanvidudam0@gmail.com>
2024-11-04 16:26:47 +0530

    retailer
    remove addrequest from admin
    add addrequest to retailer
    middleware added to retailer routes
    middleware added to tiffin item routes

--- routes/retailer.routes.ts
+++ routes/retailer.routes.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { RetailerController } from '../controllers/retailer.controller';
+import { RoleBaseValidation } from '../middleware/RoleBaseValidation';
+import { validateToken } from '../middleware/validateToken';
 
-const router = express.Router();
-const retailerRoutes = new RetailerController();
+const retailerRoutes = express.Router();
+const retailerController = new RetailerController();
+
+
+retailerRoutes.put("/addRequest/:organization_id",validateToken,RoleBaseValidation('add_request'), retailerController.addRequest);
 
 
 
-
-
-export default router;
+export default retailerRoutes;
