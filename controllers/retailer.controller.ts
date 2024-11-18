9b25fb7c5f8e5fff890b25cb6845b14eba85c5cf
ananyavic <100069309+ananyavic@users.noreply.github.com>
2024-11-18 12:23:41 +0530

    Update retailer.controller.ts

--- controllers/retailer.controller.ts
+++ controllers/retailer.controller.ts
@@ -6,12 +6,15 @@ import jwt from "jsonwebtoken";
 import { getUserFromToken } from "./admin.controller";
 import { OrderModel } from "../model/orderModel";
 import moment from "moment";
+import {
+  sendErrorResponse,
+  sendSuccessResponse,
+} from "../utils/responsesUtils";
 
 export class RetailerController {
   public addRequest = async (req: Request, res: Response) => {
     try {
       const organization_id = req.params.organization_id;
-      console.log("organization_id", req.params.id, organization_id);
 
       const token = req.headers.authorization?.split(" ")[1];
       if (token) {
@@ -19,17 +22,14 @@ export class RetailerController {
           id: string;
           role: string;
         };
-        console.log(decoded.id);
 
         const user = (await UserModel.findOne({
           _id: decoded.id,
         }).exec()) as User;
-        console.log(user);
 
         if (!user) {
-          res.status(404).json({ message: "User not found" }); // Ensure the function exits if user is not found
+          sendErrorResponse(res, 404, false, "User not found");
         }
-        console.log(decoded.id);
 
         const data = await UserModel.updateOne(
           { _id: decoded.id },
@@ -42,81 +42,43 @@ export class RetailerController {
             },
           }
         );
-        res.json(data);
+        sendSuccessResponse(res, 200, true, "Request added", data);
       }
     } catch (err) {
-      res.json(err);
+      sendErrorResponse(res, 500, false, `internal server error ${err}`);
     }
   };
 
-  
-//add pagination
+  //add pagination
   public getAllOrders = async (req: Request, res: Response) => {
     try {
       const user = await getUserFromToken(req);
       if (user?.isActive == false || !user) {
-        res.status(401).json({
-          statuscode: 401,
-          message: "Unauthorized or invalid user details.",
-        });
+        sendErrorResponse(
+          res,
+          401,
+          false,
+          "Unauthorized or invalid user details."
+        );
       } else {
         const status = req.query.status;
-        const page = parseInt(req.query.page as string) || 1;  
-        const limit = parseInt(req.query.limit as string) || 10;  
-
-        if(page < 1 || limit < 1){
-          res.status(400).json({ message: "Page and limit must be positive integers" });
-          
-        }else{
-        const skip = (page - 1) * limit;  
         let orders;
-        let totalItems;
-        let totalPages;
-
         if (status) {
           orders = await OrderModel.find({
             "cart.retailer_id": user._id,
             delivery_status: status,
-          }).skip(skip).limit(limit).exec();
-
-            totalItems = await OrderModel.countDocuments({
-            "cart.retailer_id": user._id,
-            delivery_status: status,
           });
-
-           totalPages = Math.ceil(totalItems / limit);
-
         } else {
-          orders = await OrderModel.find({ "cart.retailer_id": user._id })
-          .skip(skip).limit(limit).exec();
-        
-            totalItems = await OrderModel.countDocuments({
-            "cart.retailer_id": user._id,
-          });
-
-           totalPages = Math.ceil(totalItems / limit);
-          
+          orders = await OrderModel.find({ "cart.retailer_id": user._id });
         }
-        if (orders.length > 0)
-          res.status(200).json({ statuscode: 200, data: orders ,
-            pagination: {
-              currentPage: page,
-              totalPages: totalPages,
-              totalItems: totalItems,
-          },
-        });
-        else
-          res
-            .status(404)
-            .json({ statuscode: 404, message: "orders not found" });
+        if (orders.length > 0) {
+          sendSuccessResponse(res, 200, true, "All the orders", orders);
+        } else {
+          sendErrorResponse(res, 404, false, "orders not found");
+        }
       }
-    }
-
-       
     } catch (error) {
-      res
-        .status(500)
-        .json({ statuscode: 500, message: `internal server error ${error}` });
+      sendErrorResponse(res, 500, false, `internal server error ${error}`);
     }
   };
 
@@ -124,10 +86,12 @@ export class RetailerController {
     try {
       const user = await getUserFromToken(req);
       if (user?.isActive == false || !user) {
-        res.status(401).json({
-          statuscode: 401,
-          message: "Unauthorized or invalid user details.",
-        });
+        sendErrorResponse(
+          res,
+          401,
+          false,
+          "Unauthorized or invalid user details."
+        );
       } else {
         const status = req.query.status;
         const year = parseInt(req.query.year as string);
@@ -199,30 +163,26 @@ export class RetailerController {
               .startOf("isoWeek")
               .format("MMM Do YY");
 
-              
-              const endOfWeek = moment()
+            const endOfWeek = moment()
               .year(item._id.year)
               .isoWeek(item._id.week + 1)
               .endOf("isoWeek")
               .format("MMM Do YY");
-            
+
             return {
               startOfWeek: startOfWeek,
-              endOfWeek:endOfWeek,
+              endOfWeek: endOfWeek,
               totalOrders: item.totalOrders,
               totalAmount: item.totalAmount,
             };
           });
-          res.status(200).json({ statuscode: 200, data: data });
-        } else
-          res
-            .status(404)
-            .json({ statuscode: 404, message: "orders not found" });
+          sendSuccessResponse(res, 200, true, " ", data);
+        } else {
+          sendErrorResponse(res, 404, false, "orders not found");
+        }
       }
     } catch (error) {
-      res
-        .status(500)
-        .json({ statuscode: 500, message: `internal server error ${error}` });
+      sendErrorResponse(res, 500, false, `internal server error ${error}`);
     }
   };
 
@@ -230,10 +190,12 @@ export class RetailerController {
     try {
       const user = await getUserFromToken(req);
       if (user?.isActive == false || !user) {
-        res.status(401).json({
-          statuscode: 401,
-          message: "Unauthorized or invalid user details.",
-        });
+        sendErrorResponse(
+          res,
+          401,
+          false,
+          "Unauthorized or invalid user details."
+        );
       } else {
         const status = req.query.status;
         const year = parseInt(req.query.year as string);
@@ -248,80 +210,75 @@ export class RetailerController {
                 delivery_status: status,
                 org_created_at: {
                   $gte: startOfYear,
-                  $lte: endOfYear
-                }
-              }
+                  $lte: endOfYear,
+                },
+              },
             },
             {
               $group: {
                 _id: {
-                  year: { $year: "$org_created_at" },     
-                  month: { $month: "$org_created_at" }    
+                  year: { $year: "$org_created_at" },
+                  month: { $month: "$org_created_at" },
                 },
-                totalOrders: { $sum: 1 },                    
-                totalAmount: { $sum: "$cart.total_amount" }        
-              }
+                totalOrders: { $sum: 1 },
+                totalAmount: { $sum: "$cart.total_amount" },
+              },
             },
             {
               $sort: {
                 "_id.year": 1,
-                "_id.month": 1
-              }
-            }
-          ])	
-          
-          
-          
+                "_id.month": 1,
+              },
+            },
+          ]);
         } else {
           orders = await OrderModel.aggregate([
             {
               $match: {
                 org_created_at: {
                   $gte: startOfYear,
-                  $lte: endOfYear
-                }
-              }
+                  $lte: endOfYear,
+                },
+              },
             },
             {
               $group: {
                 _id: {
-                  year: { $year: "$org_created_at" },     
-                  month: { $month: "$org_created_at" }    
+                  year: { $year: "$org_created_at" },
+                  month: { $month: "$org_created_at" },
                 },
-                totalOrders: { $sum: 1 },                    
-                totalAmount: { $sum: "$cart.total_amount" }        
-              }
+                totalOrders: { $sum: 1 },
+                totalAmount: { $sum: "$cart.total_amount" },
+              },
             },
             {
               $sort: {
                 "_id.year": 1,
-                "_id.month": 1
-              }
-            }
-          ])	
+                "_id.month": 1,
+              },
+            },
+          ]);
         }
         if (orders) {
           data = orders.map((item) => {
-            const startOfmonth = moment().month(item._id.month-1).format('YYYY-MM');
-      
-    
-        //  const endOfWeek = startOfWeek.clone().endOf('isoWeek');
-         return { month: startOfmonth,
+            const startOfmonth = moment()
+              .month(item._id.month - 1)
+              .format("YYYY-MM");
+
+            //  const endOfWeek = startOfWeek.clone().endOf('isoWeek');
+            return {
+              month: startOfmonth,
               totalOrders: item.totalOrders,
               totalAmount: item.totalAmount,
             };
           });
           res.status(200).json({ statuscode: 200, data: data });
-        } else
-          res
-            .status(404)
-            .json({ statuscode: 404, message: "orders not found" });
+        } else {
+          sendErrorResponse(res, 404, false, "orders not found");
+        }
       }
     } catch (error) {
-      res
-        .status(500)
-        .json({ statuscode: 500, message: `internal server error ${error}` });
+      sendErrorResponse(res, 404, false, `internal server error ${error}`);
     }
   };
-
 }
