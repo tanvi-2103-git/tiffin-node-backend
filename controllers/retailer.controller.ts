fdc1ae63a6cd12eae10a2142365208a0b7b759c4
Yaminee99 <yaminee1999kushwaha@gmail.com>
2024-11-19 14:42:40 +0530

    retailer controller updated

--- controllers/retailer.controller.ts
+++ controllers/retailer.controller.ts
@@ -3,13 +3,15 @@ import { OrganizationModel, Organization } from "../model/organizationModel";
 import { UserModel, User } from "../model/userModel";
 import { ObjectId } from "mongodb";
 import jwt from "jsonwebtoken";
+import { Document } from "mongodb";
 import { getUserFromToken } from "./admin.controller";
-import { OrderModel } from "../model/orderModel";
+import { Order, OrderModel } from "../model/orderModel";
 import moment from "moment";
 import {
   sendErrorResponse,
   sendSuccessResponse,
 } from "../utils/responsesUtils";
+import { TiffinItemModel } from "../model/tiffinItemModel";
 
 export class RetailerController {
   public addRequest = async (req: Request, res: Response) => {
@@ -54,33 +56,122 @@ export class RetailerController {
     try {
       const user = await getUserFromToken(req);
       if (user?.isActive == false || !user) {
-        sendErrorResponse(
-          res,
-          401,
-          false,
-          "Unauthorized or invalid user details."
-        );
+        res.status(401).json({
+          statuscode: 401,
+          message: "Unauthorized or invalid user details.",
+        });
       } else {
         const status = req.query.status;
+        const page = parseInt(req.query.page as string) || 1;  
+        const limit = parseInt(req.query.limit as string) || 10;  
+
+        if(page < 1 || limit < 1){
+          res.status(400).json({ message: "Page and limit must be positive integers" });
+          
+        }else{
+        const skip = (page - 1) * limit;  
         let orders;
+        let totalItems;
+        let totalPages;
+        let newdata;
+
         if (status) {
           orders = await OrderModel.find({
             "cart.retailer_id": user._id,
             delivery_status: status,
+          }).skip(skip).limit(limit).exec();
+
+            totalItems = await OrderModel.countDocuments({
+            "cart.retailer_id": user._id,
+            delivery_status: status,
           });
+
+           totalPages = Math.ceil(totalItems / limit);
+
+            newdata = await this.addUserName(orders);
+
         } else {
-          orders = await OrderModel.find({ "cart.retailer_id": user._id });
+          orders = await OrderModel.find({ "cart.retailer_id": user._id })
+          .skip(skip).limit(limit).exec();
+        
+            totalItems = await OrderModel.countDocuments({
+            "cart.retailer_id": user._id,
+          });
+
+           totalPages = Math.ceil(totalItems / limit);
+            newdata = await this.addUserName(orders);
+          
         }
-        if (orders.length > 0) {
-          sendSuccessResponse(res, 200, true, "All the orders", orders);
-        } else {
-          sendErrorResponse(res, 404, false, "orders not found");
-        }
+        if (orders.length > 0)
+          res.status(200).json({ statuscode: 200, data: newdata,
+            pagination: {
+              currentPage: page,
+              totalPages: totalPages,
+              totalItems: totalItems,
+          },
+        });
+        else
+          res
+            .status(404)
+            .json({ statuscode: 404, message: "orders not found" });
       }
+    }
+
+       
     } catch (error) {
-      sendErrorResponse(res, 500, false, `internal server error ${error}`);
+      res
+        .status(500)
+        .json({ statuscode: 500, message: `internal server error ${error}` });
     }
   };
+
+  public addUserName = async (orders: Order[]) => {
+    const newdata = await Promise.all(
+      orders.map(async (order) => {
+        let updatedOrder;
+        const retailer_id = order.cart.retailer_id;
+        const customer_id = order.cart.customer_id;
+        const tiffinitems  = order.cart.items;
+        
+        const tiffindata = await Promise.all( 
+          tiffinitems.map(async(tiffinitem)=>{
+          const tiffin_id = tiffinitem.tiffin_id;
+        
+
+          const tiffin = await TiffinItemModel.findById(tiffin_id).exec();
+
+          const tiffin_name = tiffin?.tiffin_name;
+          const tiffin_type = tiffin?.tiffin_type;
+          updatedOrder = (order as Document).toObject(); 
+          const tiffinItem= (tiffinitem as Document).toObject();
+          tiffinItem.tiffin_name=tiffin_name;
+          tiffinItem.tiffin_type=tiffin_type;
+          
+          return tiffinItem
+        }));
+       
+     
+        const retailer = await UserModel.findById(retailer_id).exec();
+        const retailer_name = retailer?.username; 
+  
+       
+       const customer = await UserModel.findById(customer_id).exec();
+       const customer_name = customer?.username;
+       
+
+         updatedOrder = (order as Document).toObject(); 
+        
+         updatedOrder.cart.retailer_name = retailer_name;
+         updatedOrder.cart.customer_name = customer_name;
+         updatedOrder.cart.items = tiffindata;
+       
+         return updatedOrder;
+      })
+    );
+   // console.log(newdata);
+    return newdata;
+  };
+  
 
   public getWeeklyOrders = async (req: Request, res: Response) => {
     try {
