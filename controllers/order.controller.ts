9d1cac5cb52dfce22edd75a92f8931141178e208
ananyavic <100069309+ananyavic@users.noreply.github.com>
2024-12-03 16:36:19 +0530

    destructuring in order and retailer controller

--- controllers/order.controller.ts
+++ controllers/order.controller.ts
@@ -12,7 +12,7 @@ const cartController = new CartController();
 export class OrderController {
   public placeOrder = async (req: Request, res: Response) => {
     try {
-      const cartId = req.params.cartid;
+      const { cartid: cartId } = req.params;
       const { payment_mode } = req.body;
       let payment_status;
       let delivery_status;
@@ -30,11 +30,10 @@ export class OrderController {
             cart.items.map(async (item) => {
               return TiffinItemModel.updateOne(
                 { _id: item.tiffin_id },
-                { $inc: { tiffin_available_quantity: -item.quantity } } 
+                { $inc: { tiffin_available_quantity: -item.quantity } }
               );
             })
           );
-          
 
           const order = new OrderModel({
             cart,
@@ -43,12 +42,11 @@ export class OrderController {
             delivery_status,
           });
           await order.save();
-          
-            const cart_id = cartId;
 
-            const destroyCart = await CartModel.findByIdAndDelete(cart_id);
-            
-          
+          const cart_id = cartId;
+
+          const destroyCart = await CartModel.findByIdAndDelete(cart_id);
+
           sendSuccessResponse(res, 200, true, "Order placed", order);
         } else {
           sendSuccessResponse(
@@ -58,10 +56,7 @@ export class OrderController {
             "Please add cart or add items to cart"
           );
         }
-      } else  sendSuccessResponse(
-        res,
-        200,
-        true, "Add valid payment mode")
+      } else sendSuccessResponse(res, 200, true, "Add valid payment mode");
     } catch (error) {
       sendErrorResponse(res, 500, false, `internal server error ${error}`);
     }
@@ -69,38 +64,34 @@ export class OrderController {
 
   public confirmPayment = async (req: Request, res: Response) => {
     try {
-      const orderId = req.params.orderid;
+      const { orderid: orderId } = req.params;
+      console.log("orderId", orderId);
       const order = await OrderModel.findById(orderId).exec();
-      if(order?.delivery_status=='cancelled')  sendSuccessResponse(
-        res,
-        200,
-        true, 'order is already cancelled');
-     else{ if (order) {
-        if (order.payment_mode == "CoD") {
-          const updateOrder = await OrderModel.findByIdAndUpdate(orderId, {
-            payment_status: "paid",
-            delivery_status: "delivered",
-          });
-          // if (updateOrder) {
-          //   const cartId = order.cart._id;
-
-          //   const destroyCart = await CartModel.findByIdAndDelete(cartId);
-          //   sendSuccessResponse(res, 200, true, "Payment done");
-          // }
-        } else {
-          //in case of upi it will change after adding razorpay or neccesary payment service
-          const updateOrder = await OrderModel.findByIdAndUpdate(orderId, {
-            payment_status: "paid",
-            delivery_status: "pending",
-          });
-          if (updateOrder) {
-            sendSuccessResponse(res, 200, true, "Payment done");
+      console.log("order:  ", order);
+      if (order?.delivery_status == "cancelled") {
+        console.log("order 1:  ", order);
+        sendSuccessResponse(res, 200, true, "order is already cancelled");
+      } else {
+        if (order) {
+          console.log("order 2:  ", order);
+          if (order.payment_mode == "CoD") {
+            const updateOrder = await OrderModel.findByIdAndUpdate(orderId, {
+              payment_status: "paid",
+              delivery_status: "delivered",
+            });
+            console.log("updateorder:", updateOrder);
+          } else {
+            //in case of upi it will change after adding razorpay or neccesary payment service
+            const updateOrder = await OrderModel.findByIdAndUpdate(orderId, {
+              payment_status: "paid",
+              delivery_status: "pending",
+            });
+            if (updateOrder) {
+              sendSuccessResponse(res, 200, true, "Payment done");
+            }
           }
-        }
-      } else  sendSuccessResponse(
-        res,
-        200,
-        true, "Order not found")}
+        } else sendSuccessResponse(res, 200, true, "Order not found");
+      }
     } catch (error) {
       sendErrorResponse(res, 500, false, `internal server error ${error}`);
     }
@@ -108,7 +99,7 @@ export class OrderController {
 
   public getOrderById = async (req: Request, res: Response) => {
     try {
-      const orderId = req.params.orderid;
+      const { orderid: orderId } = req.params;
       const order = await OrderModel.findById(orderId).exec();
       if (order) {
         sendSuccessResponse(res, 200, true, `order of id: ${orderId}`, order);
